df17e1449b902572ea35b71b410827efe59b8064
nelsonGX <hi@nelsongx.com>
2025-07-01 03:11:34 +0800

    move prisma client to prevent multiple connections

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
 
 function validateApiKey(request: NextRequest) {
   const apiKey = request.headers.get('x-api-key');
